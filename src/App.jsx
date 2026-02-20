e9e84c94c3306d74a0a85505535c6a7688adbd90
Jeet Ghosh <ghoshjeet455@gmail.com>
2026-02-20 14:40:26 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import { useState } from "react";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-green-500 text-center px-2 py-4 ">Hello World</h1>
+    </>
+  );
+}
+
+export default App;
