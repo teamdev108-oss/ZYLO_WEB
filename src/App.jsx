8727e6ad9e807428786d71114cc3d3288a41e231
Souvik Sarkar <sarkorsouvo@gmail.com>
2026-02-20 18:18:54 +0530

    settings up

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { useState } from "react";
 function App() {
   return (
     <>
-      <h1 className="text-green-500 text-center px-2 py-4 ">Hello World</h1>
+      <h1 className="text-green-500 text-center px-2 py-4 ">dadur bichi</h1>
     </>
   );
 }
